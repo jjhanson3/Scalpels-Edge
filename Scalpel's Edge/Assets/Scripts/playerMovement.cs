eddca66776377934df28435eacbc42d5308bc736
jjhanson3 <jjhanson3@wisc.edu>
2021-04-06 03:51:50 -0500

    Fixed Pick Up Bugs and Added Gravity to playerMovement
    The bug for picking up items stems from the game object being in a different location than the actual model, so this led to an issue where the player would attempt to pick up an object, but no hit detector would be there.

--- Scalpel's Edge/Assets/Scripts/playerMovement.cs
+++ Scalpel's Edge/Assets/Scripts/playerMovement.cs
@@ -8,10 +8,24 @@ public class playerMovement : MonoBehaviour
     public CharacterController controller;
 
     public float speed = 8f;
+    public float gravity = -9.8f;
+
+    public Transform groundCheck;
+    public float groundDistance = 0.05f;
+    public LayerMask groundMask;
+
+    Vector3 velocity;
+    bool isGrounded;
 
     // Update is called once per frame
     void Update()
     {
+        isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
+
+        if (isGrounded && velocity.y < 0)
+        {
+            velocity.y = gravity;
+        }
 
         float x = Input.GetAxisRaw("Horizontal");
         float z = Input.GetAxisRaw("Vertical");
@@ -19,5 +33,9 @@ public class playerMovement : MonoBehaviour
         Vector3 move = transform.right * x + transform.forward * z;
 
         controller.Move(move * speed * Time.deltaTime);
+
+        velocity.y += gravity * Time.deltaTime;
+
+        controller.Move(velocity * Time.deltaTime);
     }
 }
