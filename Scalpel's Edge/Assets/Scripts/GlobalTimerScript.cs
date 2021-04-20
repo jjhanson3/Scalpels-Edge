e18a9688c6daeef3339e8ab426a593e75fc74cea
hnkoenig <43788239+hnkoenig@users.noreply.github.com>
2021-04-20 10:12:56 -0500

    Added worsening wound condition
    Wound condition now worsens over time. Spawning can be attached to this same cycle

---
+++ Scalpel's Edge/Assets/Scripts/GlobalTimerScript.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GlobalTimerScript : MonoBehaviour
+{
+    //Should new patients spawn?
+    public bool spawnActive;
+    //should patient condition worsen?
+    public bool conditionActive;
+
+    //How big the interval should be for actions.
+    //Upon creation, wounds should have an interval field
+    //representing how many intervals should pass before condition worsens.
+    //Further, new characters spawn after a given number of intervals. This is serialized for testing,
+    //but could be made constant for a final build
+    [SerializeField]
+    float interval;
+
+    private float timer = 0;
+
+    //A list of wounds to send updates to, starts empty
+    private List<GameObject> woundsList = new List<GameObject>();
+
+    //array containing all beds will be checked to find an empty one for new patients
+    public GameObject[,] beds = new GameObject[3, 4];
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //maybe initialize the patiets to spawn for a level here?
+        //Not sure what else should go here
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(timer < interval)
+        {   
+            //simply increment timer
+            timer += Time.deltaTime;
+        }
+        else
+        {
+            //reset timer
+            timer = 0;
+            if (conditionActive)
+            {
+                foreach(GameObject wound in woundsList)
+                {
+                    //call interval update in wound
+                    wound.GetComponent<Wound>().IntervalUpdate();
+                }
+            }
+            if (spawnActive)
+            {
+                //choose location
+
+                //create at location
+
+                
+            }
+        }
+    }
+
+    //Simple method to allow wounds to add themselves to the update list
+    public void AddWound(GameObject wound)
+    {
+        woundsList.Add(wound);
+    }
+
+    //simple method to allow wounds to be removed on disable
+    public void RemoveWound(GameObject wound)
+    {
+        woundsList.Remove(wound);
+    }
+}
--- Scalpel's Edge/Assets/Scripts/Wound.cs
+++ Scalpel's Edge/Assets/Scripts/Wound.cs
@@ -7,16 +7,35 @@ public class Wound : MonoBehaviour
 
     public string woundType;
 
+    //number to subract severity from to calculate intervals
+    public int tempInverse;
+
+    //severity is currently assigned through inspector for testing purposes
+    public int severity;
+    //interval count for each stage will be found by "tempInverse - severity"
+    //but is subject to change
+    private int intervals;
+
     // Start is called before the first frame update
     void Start()
     {
-        //GameObject player = GameObject.Find("Player");
+        GameObject player = GameObject.Find("Player");
         //itemPickUpScript playerScript = player.GetComponent<itemPickUpScript>();
+        intervals = tempInverse - severity;
+        player.GetComponent<GlobalTimerScript>().AddWound(gameObject);
     }
 
     // Update is called once per frame
     void Update()
     {
+        
+    }
+
+    //on disable called when object is set to inactive
+    private void OnDisable()
+    {
+        GameObject player = GameObject.Find("Player");
+        player.GetComponent<GlobalTimerScript>().RemoveWound(gameObject);
 
     }
 
@@ -37,6 +56,18 @@ public class Wound : MonoBehaviour
         }
 
         return woundType;
+    }
+
+    public void IntervalUpdate()
+    {
+        intervals--;
+        if (intervals == 0)
+        {
+            severity++;
+            //call function to update nearby clipboard info
+            intervals = tempInverse - severity;
+        }
+        Debug.Log("Wound Severity: " + severity);
     }
 }
 
