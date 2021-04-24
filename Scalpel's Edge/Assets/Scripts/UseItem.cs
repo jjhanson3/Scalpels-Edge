8e5d2bcc622bd5bcc19d2f2aa9a6ff4a2bfa331d
aks421 <61337846+aks421@users.noreply.github.com>
2021-04-23 20:46:04 -0500

    Latest (with item applying fixed)

--- Scalpel's Edge/Assets/Scripts/UseItem.cs
+++ Scalpel's Edge/Assets/Scripts/UseItem.cs
@@ -44,6 +44,7 @@ public class UseItem : MonoBehaviour
                 } else
                 {
                     print("WoundType is Null");
+                    woundType="";
                 }
 
 
